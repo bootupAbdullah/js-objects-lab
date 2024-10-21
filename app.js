19b0417f9a310b684a6b2058cfe35e3187fecf0a
Abdulla Durrani <Abdullahkd@gmail.com>
2024-10-20 20:46:34 -0400

    completed excercise 1-7

--- app.js
+++ app.js
@@ -18,18 +18,91 @@ const game = {
       { name: "rare candy", quantity: 99 },
     ],
   }
-  console.dir(pokemon, { maxArrayLength: null })
-  console.log(game)
-
-
+//   console.dir(pokemon, { maxArrayLength: null })
+  
+  
   /*
-Exercise 3
-1. Add a new property to the `game` object. Let's call it "difficulty".
-2. Choose a value for "difficulty" that you think fits the game. Ex: "Easy", "Med" or "Hard". How would you assign it?
+  Exercise 3
+  1. Add a new property to the `game` object. Let's call it "difficulty".
+  2. Choose a value for "difficulty" that you think fits the game. Ex: "Easy", "Med" or "Hard". How would you assign it?
+  
+  
+  Solve Exercise 3 here:
+  */
+ 
+ game.difficulty = ["easy"]
+
+ 
+
+/*
+Exercise 4
+1. Select a starter Pokémon from the `pokemon` array. Remember, a starter Pokémon's `starter` property is true.
+2. Add this Pokémon to the `game.party` array. Which array method will you use to add them?
 
 
-Solve Exercise 3 here:
+Solve Exercise 4 here:
+*/
+
+
+const starterPokemon = pokemon[1]
+game.party.push(starterPokemon);
+
+
+
+/*
+Exercise 5
+1. Choose three more Pokémon from the `pokemon` array and add them to your party.
+2. Consider different attributes like 'type' or 'HP' for your selection. Which array method will you use to add them?
+
+
+Solve Exercise 5 here:
+*/
+
+// I am creating three seperate varaibles to hold the (3) 
+// !! come back to this one, the variable here is not capturing the right pokemon, it is catching the one before the requested property
+const caterpie = pokemon[9]
+const abra = pokemon[62]
+const grimer = pokemon[87]
+game.party.push(caterpie, abra, grimer);
+
+
+/*
+Exercise 6
+1. Set the `completed` property to true for gyms with a difficulty below 3.
+2. Think about how you'd loop through the `gyms` array to check and update the `completed` property.
+
+
+Solve Exercise 6 here:
+*/
+
+game.gyms.forEach((gym) => {
+    if(gym.difficulty < 3) {
+        gym.completed = true
+    }
+} 
+)
+
+
+
+
+
+/*
+Exercise 7
+1. Evolve the starter Pokémon you added to your party earlier. Each starter Pokémon evolves into a specific one.
+2. How would you replace the current starter Pokémon in your party with its evolved form?
+
+Hint: 
+  - Pokemon 1: Bulbasaur evolves into Pokemon 2: Ivysaur
+  - Pokemon 4: Charmander evolves into Pokemon 5: Charmeleon
+  - Pokemon 7: Squirtle evolves into Pokemon 8: Wartortle
+  - Pokemon 25: Pikachu evolves into Pokemon 26: Raichu
+
+More Hints: The existing starter Pokemon will be *replaced* in your party with the Pokemon it evolved into. When working with an array of objects, the splice() array method is ideal for replacing one element with another. 
+
+
+Solve Exercise 7 here:
 */
 
 
 
+console.log(game)
---
+++ practice.js
@@ -0,0 +1,17 @@
+const pokemon = require('./data.js')
+
+
+const music = { currentTrack: 'Just Ken',
+    volme: 70,
+ };
+
+ music.currentPlaylist = ['Just Ken', 'Hey Blondie', 'What Was I Made For', 'Dance The Night'];
+ 
+ console.log(music);
+
+const myMusic = {
+
+}
+
+
+
